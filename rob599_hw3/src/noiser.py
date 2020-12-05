548b7db95bd0ee1ececb74d770b7b5cdf1fabfde
Alan G. Sanchez <sanchala@oregonstate.edu>
2020-12-05 14:34:33 -0800

    Add noiser in forloop

--- rob599_hw3/src/noiser.py
+++ rob599_hw3/src/noiser.py
@@ -3,13 +3,11 @@
 # Import what we need.
 import rospy
 import numpy as np
-from math import pi, atan, sin, cos
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Point, Pose
-from visualization_msgs.msg import Marker
 
-class Median_filter:
+from sensor_msgs.msg import LaserScan
+
+
+class Noiser:
 	def __init__(self):
 		# Initialize subscriber
 		self.laser_sub = rospy.Subscriber('/base_scan', LaserScan, self.laser_callback)
@@ -18,26 +16,21 @@ class Median_filter:
 		self.median_filter_pub = rospy.Publisher('/median_filter', LaserScan, queue_size=1)
 
 
-	def laser_callback(self, msg, sigma=0.01):
-		# Create a LaserScan message named "scan".
-		scan = msg
+	def laser_callback(self, scan_msg, sigma=0.01):
+		noise_list = []
+		for i in range(len(scan_msg)):
+			noise_list.append(scan_msg[i] + gauss(0.0,sigma))
 
-		self.median_filter_pub.publish(scan)
+		scan_msg.ranges = noise_list
+		self.median_filter_pub.publish(scan_msg)
 
 
-	def generate_sensor_data(n, sigma=0.01):
-	    """
-	    Generate data from a fake sensor.  These data are generated according to a sine function, with zero-mean
-	    Gaussian noise, with standard deviation sigma.
-	    """
-	    for i in range(n):
-	        yield sin(i * 0.01) + gauss(0.0, sigma)
 
 
 if __name__ == '__main__':
 	# Initialize the node
- 	rospy.init_node('median_filter')
-	Median_filter()
+ 	rospy.init_node('noiser')
+	Noiser()
 
 	# Keep running node
 	rospy.spin()
