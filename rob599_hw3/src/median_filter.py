f0da6ed94ec8f5519c9d8bf74169d7b34339ff24
Alan G. Sanchez <sanchala@oregonstate.edu>
2020-12-05 14:29:24 -0800

    Add for loop to laser_callback

--- rob599_hw3/src/median_filter.py
+++ rob599_hw3/src/median_filter.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Import what we need.
 import rospy
 import numpy as np
-from math import pi, atan, sin, cos
-from geometry_msgs.msg import Twist
+
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Point, Pose
-from visualization_msgs.msg import Marker
+
 
 class Median_filter:
 	def __init__(self):
@@ -18,11 +16,18 @@ class Median_filter:
 		self.median_filter_pub = rospy.Publisher('/median_filter', LaserScan, queue_size=1)
 
 
-	def laser_callback(self, msg):
+	def laser_callback(self, scan_msg):
 		# Create a LaserScan message named "scan".
-		scan = msg
-
-		self.median_filter_pub.publish(scan)
+		output_list =[]
+		for i in range(len(scan_msg)):
+			if i == 0 or i == len(scan_msg):
+				output_list.append(np.inf)
+				pass
+			else:
+				_,m,_=sorted([scan_msg[i-1], scan_msg[i], scan_msg[i+1]])
+				output_list.append(m)
+		scan_msg.ranges = output_list
+		self.median_filter_pub.publish(scan_msg)
 
 if __name__ == '__main__':
 	# Initialize the node
