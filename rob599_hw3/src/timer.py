8027ec4a86c8e78196363c18c1144da6982e2d23
Alan G. Sanchez <sanchala@oregonstate.edu>
2020-12-07 22:11:28 -0800

    Create timer.py script

--- rob599_hw3/src/timer.py
+++ rob599_hw3/src/timer.py
@@ -1,36 +1,83 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Import what we need.
 import rospy
+import time
+import pickle
+import os
 import numpy as np
 
 from sensor_msgs.msg import LaserScan
-
+from std_msgs.msg import Header
+from math import pi
 
 class Timer:
 	def __init__(self):
-		# Initialize subscriber
-		self.laserscan_pub = rospy.Subscriber('/base_scan', LaserScan, self.laser_callback)
-
 		# Initialize publishers
-		self.median_filter_pub = rospy.Publisher('/median_filter', LaserScan, queue_size=1)
+		self.laserscan_pub = rospy.Publisher('/scan', LaserScan, queue_size=5)
 
+		# Acquire the median_filter_size parameter
+		self.topic = rospy.get_param("topic_scan")
 
-	def laser_callback(self, scan_msg, sigma=0.01):
-		noise_list = []
-		for i in range(len(scan_msg)):
-			noise_list.append(scan_msg[i] + gauss(0.0,sigma))
+		# Initialize subscribers
+		self.laserscan_sub	   = rospy.Subscriber(self.topic, LaserScan, self.laser_callback)
 
-		scan_msg.ranges = noise_list
-		self.median_filter_pub.publish(scan_msg)
+		# Initialize scan message
+		self.scan = LaserScan()
+		self.scan.angle_min = -30 * (pi/180)
+		self.scan.angle_max =  30 * (pi/180)
+		self.scan.angle_increment = 1 * (pi/180)
+		self.scan.range_min = 0
+		self.scan.range_max = 10
+		scan_len = int((self.scan.angle_max - self.scan.angle_min)/self.scan.angle_increment)
+		self.scan.ranges = np.random.uniform(low=self.scan.angle_min, high=self.scan.angle_max, size=(scan_len,))
 
+		# Get the directory path of the script file. This pickle file will be saved here
+		self.dir_path = os.path.dirname(os.path.realpath(__file__))
+
+		# Intialize start time
+		self.start_time = None
+
+		# Intialize end time
+		self.end_time = None
+
+		# Intialize list of times
+		self.list_times = []
+
+	def publisher_scan_msg(self):
+		# Get time before the scan is published
+		self.start_time = time.time()
+		self.laserscan_pub.publish(self.scan)
+
+	def laser_callback(self,msg):
+		# Get the time for when subscriber gets
+		self.end_time = time.time()
+		self.list_times.append(self.end_time - self.start_time)
+
+	def average_time(self):
+		avg = sum(self.list_times)/100
+		print("Average Time: {0}".format(avg))
+		with open(self.dir_path + '/time.pickle', 'wb') as handle:
+				pickle.dump(avg, handle, protocol=None)
+				print("The data structure has been saved")
+		with open('time.pickle', 'rb') as handle:
+				time = pickle.load(handle)
+				print(time)
+		del self.list_times[:]
 
 
 
 if __name__ == '__main__':
 	# Initialize the node
- 	rospy.init_node('noiser')
-	Timer()
+	rospy.init_node('timer')
 
-	# Keep running node
-	rospy.spin()
+	func = Timer()
+
+	# Manage the rate.
+	rate = rospy.Rate(60)
+
+	for i in range(100):
+		func.publisher_scan_msg()
+		rate.sleep()
+
+	func.average_time()
